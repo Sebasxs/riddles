22869c8ea0693eb8a1cfa6ae53c59ff3d802b78c
Sebas Perez <32261577+Sebasxs@users.noreply.github.com>
2024-06-29 02:03:51 -0500

    Update 2023-globant-riddle-2.js
    New math approach

--- scripts/2023-globant-riddle-2.js
+++ scripts/2023-globant-riddle-2.js
@@ -9,39 +9,46 @@ What is the smallest natural number where the result of moving the digit on the 
 # ANSWER
 
 Denotation:
-- NUMBER: the BigInt we are looking for.
-- RESULT: the result of both operations performed on 'NUMBER'.
+- N: the BigInt we are looking for.
+- A: digits of N without the last one.
+- L: lenght of N.
+- R: the result of both operations performed on 'N'.
 
 Constraints:
-- Both 'NUMBER' and 'RESULT' must have the same lenght, because the transformation is just an rearrangement.
+- Both 'N' and 'R' must have the same lenght, because the transformation is just an rearrangement.
 
 Analysis:
 - Any number that maintains its length when multiplied by 9 starts with 1s or 0s, such as 1, 10, 11, 100...
-- The product of 9 and any number whose first two digits are 1 or 0 always starts with 9 ('RESULT').
-- Since 'RESULT' starts with 9, 'NUMBER' must end with 9.
-- Since 'NUMBER' ends with 9, 'NUMBER' must start with '10' (zero is forced as second digit in order to maintain the result's length)
-- The product of 9 and any number that starts with 10 and ends with 9 (109, 1009...) always ends with 1 (981, 9081...),
-  therefore the last two digits of 'NUMBER' are 19.
-- Now 'NUMBER' has the form 10...19 and 'RESULT' 910...1
-- Repeating the product of 9, we found new digits on every iteration*/
+- The product of 9 and any number whose first two digits are 1 or 0 always starts with 9 ('R').
+- Since 'R' starts with 9, 'N' must end with 9.
+
+N = 10A + 9
+R = 9N
+R = (9 * 10^(L-1)) + A
+
+Substitution:
+9(10A + 9) = (9 * 10^(L-1)) + A
+90A + 81 = (9 * 10^(L-1)) + A
+89A = (9 * 10^(L-1)) - 81
+89A = 9 * (10^(L-1) - 9)
+A = (9 * (10^(L-1) - 9))/89
+
+What next?
+- Find the first value for 'L' such that 'A' is a positive integer.
+
+*/
 
 const t0 = performance.now();
 
-const firstDigits = '10';
-const farRightDigit = '9';
-let bodyDigits = '';
-let digitCounter = 1;
-let notFound = true;
-
-while (notFound) {
-   const number = BigInt(firstDigits + bodyDigits + farRightDigit);
-   const multiplied = number * 9n;
-   const expected = BigInt(farRightDigit + firstDigits + bodyDigits);
-   bodyDigits = multiplied.toString().slice(-digitCounter);
-   notFound = multiplied !== expected;
-   digitCounter++;
-   console.log({ multiplied, expected, number });
-};
+let L = 1n;
+const numerator = (L) => 9n * (10n ** (BigInt(L) - 1n) - 9n);
+const isPositiveInteger = (L) => numerator(L) % 89n === 0n;
+while (!isPositiveInteger(L)) L++;
+const A = (9n * (10n**(L-1n) - 9n))/89n;
+const N = (10n * A) + 9n;
+const R1 = 9n * N;
+const R2 = (9n * 10n**(L-1n)) + A;
+console.log({ L, N, R1, R2, isEqual: R1 === R2 });
 
 const t1 = performance.now();
 console.log(`Time: ${t1-t0}`);
